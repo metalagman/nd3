515a2bfe0fda75adf02e6fa8f7c99688d6e4ec4a
Alexey <alexey.samoylov@gmail.com>
2016-12-16 00:25:37 +0700

    implemented task

--- 3.3/api_rest.js
+++ 3.3/api_rest.js
@@ -61,13 +61,135 @@ class UserApi {
     }
 }
 
+class TaskApi {
+    index(req, res) {
+        console.log('api index');
+        Task.find({}, function(err, data) {
+            if (err) {
+                res.status(500).send('Api error: ' + err.toString());
+            } else {
+                res.json(data);
+            }
+        });
+    };
+
+    create(req, res) {
+        console.log('api post');
+        let model = new Task(req.body);
+        model.save(function(err) {
+            if (err) {
+                res.status(500).send('Api error: ' + err.toString());
+            } else {
+                res.status(201).json(model);
+            }
+        });
+    }
+
+    update(req, res) {
+        console.log('api update');
+        Task.findById(req.params.id, function(err, model) {
+            if (err) {
+                res.status(500).send('Api error: ' + err.toString());
+            } else {
+                model.name = req.body.name;
+                model.description = req.body.description;
+                model.is_open = req.body.is_open;
+                model.user_id = req.body.user_id;
+                model.save(function(err) {
+                    if (err) {
+                        res.status(500).send('Api error: ' + err.toString());
+                    } else {
+                        res.json(model);
+                    }
+                });
+            }
+        });
+    }
+
+    remove(req, res) {
+        console.log('api remove');
+        Task.findByIdAndRemove(req.params.id, function(err) {
+            if (err) {
+                res.status(500).send('Api error: ' + err.toString());
+            } else {
+                res.status(204).send();
+            }
+        });
+
+    }
+}
+
+const openTask =  (req, res) => {
+    Task.findById(req.params.task_id, function(err, model) {
+        if (err) {
+            res.status(500).send('Api error: ' + err.toString());
+        } else {
+            model.is_open = true;
+            model.save(function(err) {
+                if (err) {
+                    res.status(500).send('Api error: ' + err.toString());
+                } else {
+                    res.json(model);
+                }
+            });
+        }
+    });
+};
+
+const closeTask =  (req, res) => {
+    Task.findById(req.params.task_id, function(err, model) {
+        if (err) {
+            res.status(500).send('Api error: ' + err.toString());
+        } else {
+            model.is_open = false;
+            model.save(function(err) {
+                if (err) {
+                    res.status(500).send('Api error: ' + err.toString());
+                } else {
+                    res.json(model);
+                }
+            });
+        }
+    });
+};
+
+const assignTask =  (req, res) => {
+    Task.findById(req.params.task_id, function(err, model) {
+        if (err) {
+            res.status(500).send('Api error: ' + err.toString());
+        } else {
+            model.user_id = req.params.user_id;
+            model.save(function(err) {
+                if (err) {
+                    res.status(500).send('Api error: ' + err.toString());
+                } else {
+                    res.json(model);
+                }
+            });
+        }
+    });
+};
+
 module.exports = (config) => {
     mongoose.connect(config.mongo.db);
+
     let userApi = new UserApi();
+    let taskApi = new TaskApi();
+
     router.get('/users', userApi.index.bind(userApi));
     router.post('/users', userApi.create.bind(userApi));
     router.put('/users/:id', userApi.update.bind(userApi));
     router.delete('/users/:id', userApi.remove.bind(userApi));
+
+    router.get('/tasks', userApi.index.bind(userApi));
+    router.post('/tasks', userApi.create.bind(userApi));
+    router.put('/tasks/:id', userApi.update.bind(userApi));
+    router.delete('/tasks/:id', userApi.remove.bind(userApi));
+
+    router.post('/tasks/:task_id/open', openTask);
+    router.post('/tasks/:task_id/close', closeTask);
+    router.post('/tasks/:task_id/assign/:user_id', assignTask());
+
     return router;
 };
 
